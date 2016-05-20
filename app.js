502e7abe19d1aeb6b7c0ccaf5f35ac2d4ba19b84
Kofi Sagoe Opoku <kso2110@columbia.edu>
2016-05-20 01:28:40 -0700

    Recording-Server Init
    Changing Node.js/socket.io chat room into a recording server.

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+// Variables for the Recording-Server
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3001;
+
+var wav = require('wav');
+var concertFile = 'demo.wav'
+
+
+// URI handlers
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
+
+
+// Handling a client
+io.on('connection', function (socket) {
+	// Log information
+	console.log('A new client has connected.');
+
+	// Creating a new FileWriter to save the concert to disk
+	var fileWriter = new wav.FileWriter(concertFile, {
+    	channels: 1,
+    	sampleRate: 48000,
+    	bitDepth: 16
+  	});
+
+  	// Store the client socket stream as a wav file
+  	socket.on('stream', function(data) {
+    	console.log('A client is sending a new stream.');
+    	console.log('Recording...');
+    	//data.pipe(fileWriter);
+  	});
+
+  	// End the client socket stream
+	socket.on('end', function() {
+  		fileWriter.end();
+  		console.log('Recording saved!');
+	});
+
+	// A client disconnected
+	socket.on("disconnect", function () {
+        console.log("A client has disconnected.");
+    });
 });
-http.listen(port, function(){
- console.log('listening on ' + port);
+
+
+// Starting the server
+server.listen(port, function(){
+ console.log('Recording-Sever listining on port: ' + port);
 });
