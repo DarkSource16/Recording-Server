3da6f1add11a30a011825592e944e3b89f6f656f
Kofi Sagoe Opoku <kso2110@columbia.edu>
2016-06-19 17:16:17 -0700

    Switching Web Socket Tech Stack
    Switching the underlying web socket tech stack from Socket.IO to
    BinaryJS.

--- app.js
+++ app.js
@@ -1,52 +1,58 @@
-// Variables for the Recording-Server
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3001;
 
+// Variables for the Recording-Server
+var express = require('express');
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
 var wav = require('wav');
-var concertFile = 'demo.wav'
+
+var port = 3700;
+var outFile = 'Concert.wav';
+var app = express();
+
+app.set('views', __dirname + '/public/jade');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'))
 
 
 // URI handlers
 app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
+app.listen(port);
+
+console.log('Recording-Sever listining on port: ' + port);
+
+binaryServer = BinaryServer({port: 9001});
 
 // Handling a client
-io.on('connection', function (socket) {
-	// Log information
-	console.log('A new client has connected.');
+binaryServer.on('connection', function(client) {
 
-	// Creating a new FileWriter to save the concert to disk
-	var fileWriter = new wav.FileWriter(concertFile, {
-    	channels: 1,
-    	sampleRate: 48000,
-    	bitDepth: 16
-  	});
+  // Log information
+  console.log('A new venue has connected.');
 
-  	// Store the client socket stream as a wav file
-  	socket.on('stream', function(data) {
-    	console.log('A client is sending a new stream.');
-    	console.log('Recording...');
-    	//data.pipe(fileWriter);
-  	});
+  // Creating a new FileWriter to save the concert to disk
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 48000,
+    bitDepth: 16
+  });
 
-  	// End the client socket stream
-	socket.on('end', function() {
-  		fileWriter.end();
-  		console.log('Recording saved!');
-	});
+  // Store the client socket stream as a wav file
+  client.on('stream', function(stream, meta) {
+    console.log('A venue is ready to record...');
+    stream.pipe(fileWriter);
 
-	// A client disconnected
-	socket.on("disconnect", function () {
-        console.log("A client has disconnected.");
+    // End the client socket stream
+    stream.on('end', function() {
+      fileWriter.end();
+      console.log('Recording saved!');
     });
-});
+  });
 
-
-// Starting the server
-server.listen(port, function(){
- console.log('Recording-Sever listining on port: ' + port);
+  // A client disconnected
+  client.on("close", function () {
+      console.log("A venue has disconnected.");
+  });
 });
